2ef9a848187160a5cda479aca5512d4e302ce9be
Geezlyjen <72477041+talithaaulia@users.noreply.github.com>
2023-10-31 19:01:55 +0700

    Add files via upload

--- app/news-detail.js
+++ app/news-detail.js
@@ -1,4 +1,4 @@
-import { Heading, Text, Center } from "@gluestack-ui/themed";
+import { Heading, Text, Center, ScrollView, Image } from "@gluestack-ui/themed";
 import { Header } from "../components";
 import { useLocalSearchParams } from "expo-router";
 
@@ -7,10 +7,21 @@ const NewsDetail = () => {
   return (
     <>
       <Header title={"News"} withBack={true} />
-      <Center flex={1} padding={"$4"}>
-        <Heading>News Detail</Heading>
-        <Text textAlign="center">{params.title}</Text>
-      </Center>
+      <ScrollView>
+        <Center flex={1} padding={"$4"}>
+          <Heading>{params.title}</Heading>
+          <Image
+            source= {{uri: params.image}}
+            alt="Image Data"
+            size="2xl"
+            mt={4}
+          />
+          <Text fontSize="$sm" color="$light500" mt={4}>
+            Date: {params.date}
+          </Text>
+          <Text mt={4}>{params.content}</Text>
+        </Center>
+      </ScrollView>
     </>
   );
 };
